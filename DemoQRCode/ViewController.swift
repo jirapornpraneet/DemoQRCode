bb8bcf05b67156a0d27ac768d02d8b2b21023ce9
iOS Dev <jiraporn.p@piesoft.net>
2021-11-08 19:32:48 +0700

    add config.plist

--- DemoQRCode/ViewController.swift
+++ DemoQRCode/ViewController.swift
@@ -24,7 +24,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        passUserDataIfNeeded()
+        encodeIfNeeded()
     }
     
     func passUserDataIfNeeded() {
